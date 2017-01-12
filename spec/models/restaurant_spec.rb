4b5a40e85f1ee94ca46ba30c83e0913bbabd9647
Dan Pelensky <dan@pelensky.com>
2017-01-12 11:44:35 +0000

    withuserassociationextension created

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,5 +6,19 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+end
 
+describe 'reviews' do
+  describe 'build_with_user' do
+    let(:user) { User.create email: 'test@test.com' }
+    let(:restaurant) { Restaurant.create name: 'Test' }
+    let(:review_params) { {rating: 5, thoughts: 'yum'} }
+    subject(:review) { restaurant.reviews.build_with_user(review_params, user) }
+    it 'builds review' do
+      expect(review).to be_a Review
+    end
+    it 'builds a review associated with the specified user' do
+      expect(review.user).to eq user
+    end
+  end
 end

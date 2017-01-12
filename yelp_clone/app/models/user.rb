eb518bcfdd0b1edb03404c79b7e8d6354c0873b1
Asuka Ochi <asukaochi@gmail.com>
2017-01-12 16:49:27 +0000

    set correct association between restaurant to reviews

--- yelp_clone/app/models/user.rb
+++ yelp_clone/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :restaurants
+  has_many :reviewed_restaurants, through: :reviews, source: :restaurant
   has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
